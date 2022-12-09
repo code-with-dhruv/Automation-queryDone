bf9f411913f90442de9c075a6d864aa1dae81eff
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-12-09 23:22:31 +0530

    Delete Hello directory

--- Hello/home/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from home.models import Excel
-# Register your models here.
-admin.site.register(Excel)
--- Hello/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from home import views
-urlpatterns = [
-    path("",views.index,name='home'),
-    path("search",views.search,name='search'),
-    path("venue_csv",views.venue_csv,name='venue_csv'),
-    path("categories",views.categories,name='categories'),
-]
